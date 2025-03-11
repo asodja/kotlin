c5fd65eac4a040774ffb1298ac7016fdea6c29d6
Anže Sodja <asodja@gradle.com>
2025-03-11 11:55:48 +0100

    Setup performance

---
+++ profile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+currentDir=$(pwd)
+"$currentDir/../gradle-profiler/build/install/gradle-profiler/bin/gradle-profiler" --profile jprofiler \
+    --jprofiler-alloc \
+    --jprofiler-heapdump \
+    --jprofiler-home="/Applications/JProfiler.app/" \
+    --studio-install-dir="/Applications/Android Studio Meerkat.app" \
+    --studio-sandbox-dir="$currentDir/profile-out/studio-sandbox" \
+    --gradle-version="9.0-branch-provider_api_migration_public_api_changes-20250304171043+0000" \
+    --project-dir="$currentDir" \
+    --scenario-file "$currentDir/scenario.config" "runSync"
